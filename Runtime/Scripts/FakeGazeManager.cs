21bb1e19aee448c0de6879f8cb8781f46ffb33ac
Mael Lacour <public.dev@maellacour.com>
2022-10-27 09:45:37 +0200

    Implement interface in FakeGazeManager

--- Runtime/Scripts/FakeGazeManager.cs
+++ Runtime/Scripts/FakeGazeManager.cs
@@ -13,8 +13,6 @@
 	{
 		[Header("Settings")]
 		public bool ScaleAffectedByPrecision;
-		[SerializeField, Tooltip("Enable the gaze visualizer to highlight the gaze position.")]
-		private bool _gazeVisualFeedback;
 		[SerializeField] private bool _smoothMove = true;
 
 		[SerializeField][Range(1, 30)] private int _smoothMoveSpeed = 30;
@@ -33,29 +31,38 @@
 		private const float _scaleFactor = 0.03f;
 
 		//these are get
-		private Vector3 _gazeCombinedPosition;
-		private GazeRay _gazeRay;
 		private RaycastHit _gazeHit;
 
-		private GameObject currentObjectLookedAt = null;
+		private bool _isUserDetected;
 
-		#region Getters
-		public bool gazeVisualFeedback { get => _gazeVisualFeedback; set => _gazeVisualFeedback = value; }
-		public EyeData_v2 eyeData { get { return default(EyeData_v2); } }
-		public Vector3 gazeCombinedPosition { get { return _gazeCombinedPosition; } }
-		public GazeRay gazeRay { get { return _gazeRay; } }
-		public RaycastHit gazeHit { get { return _gazeHit; } }
-		public float opennessLeft { get { return 1f; } }
-		public float opennessRight { get { return 1f; } }
-		public float pupilDiameterLeft { get { return 1f; } }
-		public float pupilDiameterRight { get { return 1f; } }
+		private EyesPhysiologicalData _eyePhysiologicalData;
+
+		private GazeData _gazeData;
+
+
+		private GameObject _currentObjectLookedAt = null;
+
+		#region interface implementation
+
+		public bool isUserDetected { get { return _isUserDetected; } }
+
+		public EyesPhysiologicalData eyePhysiologicalData { get { return _eyePhysiologicalData; } }
+
+		public GazeData gazeData { get { return _gazeData; } }
 
 		/// <summary>
 		/// The object currently looked at, if any.
 		/// Note: if a parent has a rigidbody, that gameObject will be returned instead of the one having the collider.
 		/// </summary>
-		public GameObject objectLookedAt { get { return currentObjectLookedAt; } }
+		public GameObject objectLookedAt { get { return _currentObjectLookedAt; } }
 		public Action<GameObject> objectLookedChanged { get; set; }
+
+		[Header("Debugging:")]
+		[SerializeField] private bool _skipCalibration = false;
+
+		[SerializeField, Tooltip("Enable the gaze visualizer to highlight the gaze position.")]
+		private bool _gazeVisualFeedback;
+		public bool gazeVisualFeedback { get => _gazeVisualFeedback; set => _gazeVisualFeedback = value; }
 
 		#endregion
 
@@ -64,6 +71,7 @@
 			_mainCamera = Camera.main;
 
 			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_spriteRenderer.enabled = gazeVisualFeedback && _gazeData.isValid;
 		}
 
 		private void Update()
@@ -72,68 +80,66 @@
 			{
 				_mainCamera = Camera.main;
 			}
-			SetGazeRayAndEyeOpenness();
+			GetEyePhysiologicalAndGazeData();
 		}
 
-		private void SetGazeRayAndEyeOpenness()
+		private void GetEyePhysiologicalAndGazeData()
 		{
 			Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-			_gazeRay.origin = _mainCamera.transform.position;
-			_gazeRay.direction = ray.direction;
-			_gazeRay.isValid = true;
+			_gazeData.originWorld = _mainCamera.transform.position;
+			_gazeData.directionWorld = ray.direction;
+			_gazeData.isValid = true;
+			_isUserDetected = true;
 
-			if (Physics.Raycast(ray, out _gazeHit, maxDistance: _raycastMaxDistance, layerMask: _raycastLayerMask))
+			if (Physics.Raycast(ray, out _gazeData.gazeHit, maxDistance: _raycastMaxDistance, layerMask: _raycastLayerMask))
 			{
-				_gazeRay.distance = _gazeHit.distance;
-				if (_gazeHit.rigidbody != null && _gazeHit.rigidbody.gameObject != currentObjectLookedAt || _gazeHit.collider.gameObject != currentObjectLookedAt)
+				_gazeData.distance = _gazeData.gazeHit.distance;
+				if (_gazeData.gazeHit.rigidbody != null && _gazeData.gazeHit.rigidbody.gameObject != _currentObjectLookedAt || _gazeData.gazeHit.collider.gameObject != _currentObjectLookedAt)
 				{
-					currentObjectLookedAt = _gazeHit.rigidbody != null ? _gazeHit.rigidbody.gameObject : _gazeHit.collider.gameObject;
-					if (objectLookedChanged != null)
-					{
-						objectLookedChanged(currentObjectLookedAt);
-					}
+					UpdateObjectLookedAt(_gazeData.gazeHit.rigidbody != null ? _gazeData.gazeHit.rigidbody.gameObject : _gazeData.gazeHit.collider.gameObject);
 				}
 			}
 			else
 			{
-				_gazeRay.distance = _raycastMaxDistance;
-				if (currentObjectLookedAt != null)
-				{
-					currentObjectLookedAt = null;
-					if (objectLookedChanged != null)
-					{
-						objectLookedChanged(currentObjectLookedAt);
-					}
-				}
+				_gazeData.distance = _raycastMaxDistance;
+				UpdateObjectLookedAt(null);
 			}
 
 			if (gazeVisualFeedback)
 			{
-				_spriteRenderer.enabled = _gazeRay.isValid;
+				_spriteRenderer.enabled = _gazeData.isValid;
 
 				if (_spriteRenderer.enabled)
 				{
-					SetPositionAndScale(_gazeRay);
+					SetPositionAndScale(_gazeData);
 				}
-			}
-			else
-			{
-				_spriteRenderer.enabled = false;
 			}
 		}
 
-		private void SetPositionAndScale(GazeRay gazeRay)
+		private void SetPositionAndScale(GazeData gazeData)
 		{
-			Vector3 interpolatedGazeDirection = Vector3.Lerp(_lastGazeDirection, gazeRay.direction, _smoothMoveSpeed * Time.unscaledDeltaTime);
+			Vector3 interpolatedGazeDirection = Vector3.Lerp(_lastGazeDirection, gazeData.directionWorld, _smoothMoveSpeed * Time.unscaledDeltaTime);
 
-			Vector3 usedDirection = _smoothMove ? interpolatedGazeDirection.normalized : gazeRay.direction.normalized;
-			transform.position = gazeRay.origin + usedDirection * gazeRay.distance;
+			Vector3 usedDirection = _smoothMove ? interpolatedGazeDirection.normalized : gazeData.directionWorld.normalized;
+			transform.position = gazeData.originWorld + usedDirection * gazeData.distance;
 
-			transform.localScale = Vector3.one * gazeRay.distance * _scaleFactor;
+			transform.localScale = Vector3.one * gazeData.distance * _scaleFactor;
 
 			transform.forward = usedDirection.normalized;
 
 			_lastGazeDirection = usedDirection;
+		}
+
+		private void UpdateObjectLookedAt(GameObject newObject)
+		{
+			if (_currentObjectLookedAt != newObject)
+			{
+				_currentObjectLookedAt = newObject;
+				if (objectLookedChanged != null)
+				{
+					objectLookedChanged(_currentObjectLookedAt);
+				}
+			}
 		}
 	}
 }
